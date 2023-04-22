a610202f9f60f70ed034859c35a3920d0ff275c4
Rodolfo Brandão <rodolfobrandao1995@gmail.com>
2023-04-22 03:13:44 -0300

    Create IModelFactory.cs

---
+++ src/MusicLibrary.Core/Contracts/Factories/IModelFactory.cs
@@ -0,0 +1,13 @@
+using MusicLibrary.Core.Enums;
+using MusicLibrary.Core.Models;
+
+namespace MusicLibrary.Core.Contracts.Factories;
+
+public interface IModelFactory
+{
+    Artist CreateArtist(Guid genreId, string name);
+    Genre CreateGenre(string name);
+    Music CreateMusic(Guid productionId, byte ordinalPosition, string title, float durationInMinutes);
+    Production CreateProduction(Guid artistId, string title, ProductionType productionType, ushort releaseYear);
+    User CreateUser(string username, string email, string password, string passwordSalt, string role);
+}
