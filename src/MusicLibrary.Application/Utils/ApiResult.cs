515193f612e57dbd59af98cf0b5fd93c162c0c68
Rodolfo Brandão <rodolfobrandao1995@gmail.com>
2023-07-06 22:58:00 -0300

    Make constructor parameter optional
    Set default value for "(int) statusCode" to 200.

--- src/MusicLibrary.Application/Utils/ApiResult.cs
+++ src/MusicLibrary.Application/Utils/ApiResult.cs
@@ -1,12 +1,14 @@
+using Microsoft.AspNetCore.Http;
+
 namespace MusicLibrary.Application.Utils;
 
 public class ApiResult<TResponse>
 {
-    public int StatusCode { get; }
+    public int StatusCode { get; set; }
     public TResponse Response { get; set; }
     public string ErrorMessage { get; set; }
 
-    public ApiResult(int statusCode)
+    public ApiResult(int statusCode = StatusCodes.Status200OK)
     {
         StatusCode = statusCode;
         Response = default;
