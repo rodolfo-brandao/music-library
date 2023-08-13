4aaa2648e74ee259ad96bf62ce1510df745bc47c
Rodolfo Brandão <rodolfobrandao1995@gmail.com>
2023-08-13 12:37:39 -0300

    Update ListArtistsHandler.cs

--- src/MusicLibrary.Application/Queries/Artists/ListArtists/ListArtistsHandler.cs
+++ src/MusicLibrary.Application/Queries/Artists/ListArtists/ListArtistsHandler.cs
@@ -1,14 +1,15 @@
-using System.Net;
 using AutoMapper;
 using FluentValidation;
 using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using MusicLibrary.Application.Responses.Artists;
 using MusicLibrary.Application.Utils;
 using MusicLibrary.Core.Contracts.Repositories;
 
 namespace MusicLibrary.Application.Queries.Artists.ListArtists;
 
-public class ListArtistsHandler : IRequestHandler<ListArtistsQuery, ApiResult<List<DefaultArtistResponse>>>
+public class ListArtistsHandler : IRequestHandler<ListArtistsQuery, ApiResult<IEnumerable<DefaultArtistResponse>>>
 {
     private readonly IArtistRepository _artistRepository;
     private readonly IMapper _mapper;
@@ -19,36 +20,32 @@ public class ListArtistsHandler : IRequestHandler<ListArtistsQuery, ApiResult<Li
         _mapper = mapper;
     }
 
-    public async Task<ApiResult<List<DefaultArtistResponse>>> Handle(ListArtistsQuery request,
-        CancellationToken cancellationToken)
+    public async Task<ApiResult<IEnumerable<DefaultArtistResponse>>> Handle(ListArtistsQuery request, CancellationToken cancellationToken)
     {
-        var validation = await new ListArtistsValidator
-        {
-            ClassLevelCascadeMode = CascadeMode.Stop
-        }.ValidateAsync(request, cancellationToken);
+        var apiResult = new ApiResult<IEnumerable<DefaultArtistResponse>>();
+        var validation = await new ListArtistsQueryValidator().ValidateAsync(request, cancellationToken);
 
         if (!validation.IsValid)
         {
-            return new ApiResult<List<DefaultArtistResponse>>((int)HttpStatusCode.BadRequest)
-            {
-                ErrorMessage = validation.Errors.SingleOrDefault()?.ErrorMessage
-            };
+            apiResult.StatusCode = StatusCodes.Status400BadRequest;
+            apiResult.ErrorMessage = validation.Errors.Single().ErrorMessage;
         }
-
-        var artists = _artistRepository.Query(artist => !artist.IsDisabled, isReadOnly: true).ToList();
-
-        if (!string.IsNullOrWhiteSpace(request.Name))
+        else
         {
-            artists = artists.Where(artist =>
-                artist.Name.Contains(request.Name, StringComparison.OrdinalIgnoreCase)).ToList();
+            var artists = await _artistRepository.Query(artist => !artist.IsDisabled, isReadOnly: true).ToListAsync();
+
+            if (!string.IsNullOrWhiteSpace(request.Name))
+            {
+                artists = artists.Where(artist => artist.Name.Contains(request.Name, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            artists = request.OrderBy.Equals("desc", StringComparison.OrdinalIgnoreCase)
+                ? artists.OrderByDescending(artist => artist.Name).ToList()
+                : artists.OrderBy(artist => artist.Name).ToList();
+
+            apiResult.Response = _mapper.Map<IEnumerable<DefaultArtistResponse>>(artists.ToList());
         }
 
-        artists = request.OrderBy.Equals("desc", StringComparison.OrdinalIgnoreCase)
-            ? artists.OrderByDescending(artist => artist.Name).ToList()
-            : artists.OrderBy(artist => artist.Name).ToList();
-
-        var response = _mapper.Map<List<DefaultArtistResponse>>(artists.ToList());
-        var apiResult = new ApiResult<List<DefaultArtistResponse>>((int)HttpStatusCode.OK) { Response = response };
         return apiResult;
     }
 }
