63b011594e46865780ebed17f9786977506f39fa
Rodolfo Brandão <rodolfobrandao1995@gmail.com>
2023-08-13 12:37:30 -0300

    Change response type

--- src/MusicLibrary.Application/Queries/Artists/ListArtists/ListArtistsQuery.cs
+++ src/MusicLibrary.Application/Queries/Artists/ListArtists/ListArtistsQuery.cs
@@ -4,7 +4,7 @@ using MusicLibrary.Application.Utils;
 
 namespace MusicLibrary.Application.Queries.Artists.ListArtists;
 
-public class ListArtistsQuery : IRequest<ApiResult<List<DefaultArtistResponse>>>
+public class ListArtistsQuery : IRequest<ApiResult<IEnumerable<DefaultArtistResponse>>>
 {
     public string Name { get; set; } = default;
     public string OrderBy { get; set; } = "asc";
