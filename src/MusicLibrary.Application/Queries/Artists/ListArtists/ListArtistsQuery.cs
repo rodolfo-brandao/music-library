25b82bb2d83726ac3ac6d5cb378165362df8e48c
Rodolfo Brandão <rodolfobrandao1995@gmail.com>
2023-08-19 20:09:25 -0300

    Make query class inherit from its base abstraction

--- src/MusicLibrary.Application/Queries/Artists/ListArtists/ListArtistsQuery.cs
+++ src/MusicLibrary.Application/Queries/Artists/ListArtists/ListArtistsQuery.cs
@@ -1,11 +1,10 @@
 using MediatR;
+using MusicLibrary.Application.Queries.Abstract;
 using MusicLibrary.Application.Responses.Artists;
 using MusicLibrary.Application.Utils;
 
 namespace MusicLibrary.Application.Queries.Artists.ListArtists;
 
-public class ListArtistsQuery : IRequest<ApiResult<IEnumerable<DefaultArtistResponse>>>
+public class ListArtistsQuery : BasicQuery, IRequest<ApiResult<IEnumerable<DefaultArtistResponse>>>
 {
-    public string Name { get; init; } = default;
-    public string OrderBy { get; init; } = "asc";
 }
