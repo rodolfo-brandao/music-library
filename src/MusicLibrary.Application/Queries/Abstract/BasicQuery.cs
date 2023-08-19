51225fe3fec2297021c899351c2a130897313b64
Rodolfo Brandão <rodolfobrandao1995@gmail.com>
2023-08-19 20:08:42 -0300

    Add basic query abstraction

---
+++ src/MusicLibrary.Application/Queries/Abstract/BasicQuery.cs
@@ -0,0 +1,11 @@
+namespace MusicLibrary.Application.Queries.Abstract;
+
+/// <summary>
+/// Simple abstraction to provide common query properties.
+/// </summary>
+public abstract class BasicQuery
+{
+    public string Name { get; init; } = default;
+
+    public string OrderBy { get; init; } = "asc";
+}
