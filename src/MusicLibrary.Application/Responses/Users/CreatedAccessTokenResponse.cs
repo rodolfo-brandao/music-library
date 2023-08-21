5be505a1c20938d0cd6b25876e3e5e62a035fc74
Rodolfo Brandão <rodolfobrandao1995@gmail.com>
2023-08-21 19:54:12 -0300

    Add XML tag to express expiry time

--- src/MusicLibrary.Application/Responses/Users/CreatedAccessTokenResponse.cs
+++ src/MusicLibrary.Application/Responses/Users/CreatedAccessTokenResponse.cs
@@ -4,5 +4,9 @@ public class CreatedAccessTokenResponse
 {
     public string AccessToken { get; init; }
     public string TokenType { get; init; }
+    
+    /// <summary>
+    /// Expressed in seconds. 
+    /// </summary>
     public ushort ExpiresIn { get; init; }
 }
