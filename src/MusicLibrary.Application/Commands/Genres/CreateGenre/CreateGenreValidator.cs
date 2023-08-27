dee935a13faf0e0f070dab44897f45524e277696
Rodolfo Brandão <rodolfobrandao1995@gmail.com>
2023-08-27 20:27:05 -0300

    Remove unnecessary boolean literal

--- src/MusicLibrary.Application/Commands/Genres/CreateGenre/CreateGenreValidator.cs
+++ src/MusicLibrary.Application/Commands/Genres/CreateGenre/CreateGenreValidator.cs
@@ -17,7 +17,7 @@ public class CreateGenreValidator : AbstractValidator<CreateGenreCommand>
             .WithMessage(command => ValidationFailureMessages.ForEmptyProperty(nameof(command.Name)))
             .MaximumLength(MaxNameLength)
             .WithMessage(command => ValidationFailureMessages.ForExceededMaxLength(nameof(command.Name), MaxNameLength))
-            .MustAsync(async (name, _) => await genreRepository.ExistsAsync(genre => genre.Name.Equals(name)) == false)
+            .MustAsync(async (name, _) => !await genreRepository.ExistsAsync(genre => genre.Name.Equals(name)))
             .WithMessage(command =>
                 ValidationFailureMessages.ForRecordConflict("Genre", nameof(command.Name), command.Name));
     }
