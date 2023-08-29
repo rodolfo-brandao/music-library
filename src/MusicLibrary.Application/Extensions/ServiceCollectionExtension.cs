31d9171159303061154c3a7737e65333cfffc450
Rodolfo Brandão <rodolfobrandao1995@gmail.com>
2023-08-29 17:16:35 -0300

    Exclude extension class from code coverage

--- src/MusicLibrary.Application/Extensions/ServiceCollectionExtension.cs
+++ src/MusicLibrary.Application/Extensions/ServiceCollectionExtension.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.Extensions.DependencyInjection;
 using MusicLibrary.Application.Queries.Artists.ListArtists;
 using MusicLibrary.Core.Contracts.Services;
@@ -5,6 +6,7 @@ using MusicLibrary.Data.Services;
 
 namespace MusicLibrary.Application.Extensions;
 
+[ExcludeFromCodeCoverage]
 public static class ServiceCollectionExtension
 {
     public static IServiceCollection AddCustomMediatR(this IServiceCollection serviceCollection)
